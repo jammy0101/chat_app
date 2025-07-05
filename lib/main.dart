88f77d9f3652ccd7b7182d90283bcf1b0912bd15
jammy0101 <walker0101walker.gmail.com>
2025-07-05 12:01:25 +0500

    i have show the chat of user in home Screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,56 @@
+// import 'package:chat_app/screens/chat_screen.dart';
+// import 'package:chat_app/screens/login_screen.dart';
+// import 'package:chat_app/screens/registration_screen.dart';
+// import 'package:chat_app/screens/welcome_screen.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'firebase_options.dart';
+//
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//
+//   runApp(const MyApp());
+//
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return FlashChat();
+//   }
+// }
+//
+// class FlashChat extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       // theme: ThemeData.dark().copyWith(
+//       //   textTheme: const TextTheme(
+//       //     bodyMedium: TextStyle(color: Colors.black45),
+//       //   ),
+//       // ),
+//       initialRoute: WelcomeScreen.id,
+//       routes: {
+//         WelcomeScreen.id: (context) => WelcomeScreen(),
+//         'login_screen': (context) => LoginScreen(),
+//         'registration_screen': (context) => RegistrationScreen(),
+//         'chat_screen': (context) => ChatScreen(),
+//       },
+//     );
+//   }
+// }
 import 'package:chat_app/screens/chat_screen.dart';
 import 'package:chat_app/screens/login_screen.dart';
 import 'package:chat_app/screens/registration_screen.dart';
 import 'package:chat_app/screens/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
@@ -13,7 +61,6 @@ void main() async {
   );
 
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -28,6 +75,8 @@ class MyApp extends StatelessWidget {
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // theme: ThemeData.dark().copyWith(
@@ -35,12 +84,12 @@ class FlashChat extends StatelessWidget {
       //     bodyMedium: TextStyle(color: Colors.black45),
       //   ),
       // ),
-      initialRoute: WelcomeScreen.id,
+      initialRoute: user != null ? ChatScreen.id : WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
-        'login_screen': (context) => LoginScreen(),
-        'registration_screen': (context) => RegistrationScreen(),
-        'chat_screen': (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/welcome_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -22,6 +23,7 @@ class _ChatScreenState extends State<ChatScreen> {
     super.initState();
     getCurrentUser();
   }
+
   void getCurrentUser()async{
     try{
       final user = await auth.currentUser;
@@ -34,6 +36,38 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  void putMessages()async{
+    try{
+      final data = await  firestore.collection('messages').add({
+        'text' : messageText,
+        'sender' : loggedInUser!.email,
+      });
+      print(data);
+    }catch(e){
+      print('Error $e');
+    }
+  }
+
+  // void getMessages()async{
+  //   try{
+  //     final messages = await firestore.collection('messages').get();
+  //     for(var message in messages.docs){
+  //       print(message.data());
+  //     }
+  //   }catch(e){
+  //     print('Error : $e');
+  //   }
+  // }
+
+  void messageStream()async{
+    await for(var snapshot in firestore.collection('messages').snapshots()){
+      for(var message in snapshot.docs){
+              print(message.data());
+            }
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,11 +76,12 @@ class _ChatScreenState extends State<ChatScreen> {
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.close),
-              onPressed: (){
-                //Implement logout functionality
-                 auth.signOut();
-                Navigator.pop(context);
-              }),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushNamedAndRemoveUntil(context, WelcomeScreen.id, (route) => false);
+              // getMessages();
+              messageStream();
+            },),
         ],
         title: Text('⚡️Chat'),
         backgroundColor: Colors.lightBlueAccent,
@@ -56,31 +91,67 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: firestore.collection('messages').snapshots(),
+                builder: (context, snapshot) {
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // Show loader when waiting for data
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Something went wrong'));
+                  }
+
+                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                    // Show message when there is no chat data
+                    return Center(child: Text('No messages yet'));
+                  }
+
+                  final messages = snapshot.data!.docs;
+                  List<Text> messageWidgets = [];
+
+                  for (var message in messages) {
+                    final messageText = message['text'];
+                    final messageSender = message['sender'];
+                    final messageWidget = Text('$messageText from $messageSender');
+                    messageWidgets.add(messageWidget);
+                  }
+
+                  return ListView(
+                    padding: EdgeInsets.all(8),
+                    children: messageWidgets,
+                  );
+                },
+              ),
+            ),
             Container(
               decoration: kMessageContainerDecoration,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                        onChanged: (value) {
-                          messageText = value;
-                        },
-                        decoration: kMessageTextFieldDecoration
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                          onChanged: (value) {
+                            messageText = value;
+                          },
+                          decoration: kMessageTextFieldDecoration
+                      ),
                     ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      firestore.collection('messages').add({
-                        'text' : messageText,
-                        'sender' : loggedInUser!.email,
-                    });
-                    },
-                    child: Text('Send',
-                        style: kSendButtonTextStyle
-                    ),
-                  )
-                ],
+                    InkWell(
+                      onTap: (){
+                        putMessages();
+                      },
+                      child: Text('Send',
+                          style: kSendButtonTextStyle
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
